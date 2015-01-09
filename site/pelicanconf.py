4fcc27d2ffcca3b2d6b3b347ae10e9644ec863df
Diogo <diogo.mvieira@labsynapse.com>
2015-01-09 16:58:47 -0200

    changed menu and removed pagination

--- site/pelicanconf.py
+++ site/pelicanconf.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Diogo Munaro Vieira'
 SITENAME = u'Diogo Munaro Vieira'
+SITESUBTITLE = u'Because IT is Simple and Programming is Cool'
 SITEURL = 'http://diogomunaro.com'
 
 PATH = 'content'
@@ -16,16 +17,6 @@ THEME = 'crowsfoot'
 TIMEZONE = 'America/Sao_Paulo'
 
 DEFAULT_LANG = u'en'
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 5
 
@@ -42,7 +33,7 @@ CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 GITHUB_ADDRESS = 'http://github.com/dmvieira/'
 EMAIL_ADDRESS = 'diogo.mvieira@gmail.com'
 TWITTER_ADDRESS = 'https://twitter.com/diogomvieira'
-FB_ADDRESS = 'https://pt-br.facebook.com/munarovieira'
+#FB_ADDRESS = 'https://pt-br.facebook.com/munarovieira'
 LINKEDIN_ADDRESS = 'https://br.linkedin.com/in/dmvieira'
 PROFILE_IMAGE_URL = 'https://pt.gravatar.com/userimage/34518379/821607f3755e2bf394491f052ed24d2e.jpg?size=400'
 
