a73ce92f58ccf0b8775b0fb3f2255c256e85c347
Diogo Munaro <diogo.mvieira@gmail.com>
2018-06-30 12:15:51 -0300

    Life is simple and blog too ;)

--- Makefile
+++ Makefile
@@ -1,14 +1,4 @@
-.PHONY: install run build
+.PHONY: run
 
-ROOT=$(shell pwd)
-SRCFOLDER=$(ROOT)/src
-
-install:
-	@pip install -r requirements.txt
-	@git submodule update --init
-
-build:
-	@cd $(SRCFOLDER) && pelican
-
-run: build
-	@python -m SimpleHTTPServer
+run:
+	python -m SimpleHTTPServer
---
+++ theme/js/rss.js
@@ -0,0 +1,49 @@
+function mkPost(item) {
+    var template = ' \
+    <li class="post"> \
+        <header class="posttitle"> \
+        <h3><a href="' + item.link + '" \
+                rel="bookmark" \
+                class="nodec" \
+                title="' + item.title + '">'+ item.title +'</a> \
+        </h3> \
+        </header> \
+        <div class="postinfo"> \
+        <p class="published" title="'+ item.categories.join(", ") +'"> Categorias: '+ item.categories.join(", ") +' </p> \
+        </div> \
+        <div class="postsummary"> '+ $(item.description).filter("p:first").text().slice(0,-1) +'... </div> \
+    </li> \
+    ';
+    return template;
+}
+
+function getRss(url) {
+    var data = {
+        rss_url: url
+    };
+    var output = "";
+    $.get('https://api.rss2json.com/v1/api.json', data, function(response) {
+        if (response.status == 'ok') {
+            $.each(response.items, function(k, item) {
+                output += mkPost(item);
+            });
+            writePosts(output);
+        }
+    });
+    return output;
+}
+
+function writePosts(output) {
+    var $content = $('#postlist');
+    $content.append(output);
+}
+
+$(function() {
+
+    var rss = ['https://medium.com/feed/@diogomunarovieira', 'https://medium.com/feed/databootcamp']
+
+    for (var i = 0; i < rss.length; i++) {
+        getRss(rss[i]);
+    }
+
+});
